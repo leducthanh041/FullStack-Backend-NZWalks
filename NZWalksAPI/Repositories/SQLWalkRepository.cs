4d3e132244ef984846890e274b0b7d0b1c0d6086
leducthanh041 <leducthanh041282@gmail.com>
2025-09-16 15:13:31 +0700

    05. 003 Creating Walks Controller and Create New Walk Action Method ###### CRUD method (now for another route)

---
+++ NZWalksAPI/Repositories/SQLWalkRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Repositories
+{
+    public class SQLWalkRepository : IWalkRepository
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public SQLWalkRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Walk> CreateAsync(Walk walk)
+        {
+            await dbContext.Walks.AddAsync(walk);
+            await dbContext.SaveChangesAsync();
+            return walk;
+        }
+    }
+}
