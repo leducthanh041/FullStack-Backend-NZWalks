4998815646792bcb2fcb780ed7e4f1e3a9d7eafc
leducthanh041 <leducthanh041282@gmail.com>
2025-09-18 09:43:24 +0700

    07.005 Pagination

--- NZWalksAPI/Controllers/WalksController.cs
+++ NZWalksAPI/Controllers/WalksController.cs
@@ -40,13 +40,15 @@ namespace NZWalksAPI.Controllers
         }
 
         // GET Walks
-        // GET: /api/walks?filterOn=Name&filterQuery=Track&sortBy=Name&isAscending=true
+        // GET: /api/walks?filterOn=Name&filterQuery=Track&sortBy=Name&isAscending=true&pageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery,
-            [FromQuery] string? sortBy, [FromQuery] bool? isAscending)
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery, // filterOn, filterQuery
+            [FromQuery] string? sortBy, [FromQuery] bool? isAscending, // Sorting
+            [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000) // pagination
         {
             // Map DTO to Domain Model
-            var walkDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, isAscending ?? true);
+            var walkDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, isAscending ?? true,
+                pageNumber, pageSize);
 
             // Map Domain Model to DTO
             var walkDto = mapper.Map<List<WalkDto>>(walkDomainModel);
